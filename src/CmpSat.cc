d7d493ef225b44569eb1d6cefaa60a3a474f6674
Chiao Hsieh <chsieh16@illinois.edu>
2017-05-05 23:07:08 -0500

    Change to llvm-3.8.0

--- src/CmpSat.cc
+++ src/CmpSat.cc
@@ -2,6 +2,7 @@
 #include <llvm/Analysis/CFG.h>
 #include <llvm/DataLayout.h>
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/Module.h>
 #include <llvm/Pass.h>
 #include <llvm/IR/Dominators.h>
 #include <llvm/Support/raw_ostream.h>
